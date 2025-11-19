80b516bbf21cb5ffa6ad8380c08c7e3a9d6526db
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-19 20:41:27 +0530

    task 1 by Dev1

--- GitProj01/src/test/java/com/nt/main/AppTest.java
+++ GitProj01/src/test/java/com/nt/main/AppTest.java
@@ -1,5 +1,6 @@
 package com.nt.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,28 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositive() {
+        App app=new App();
+        int expected=30;
+        int actual=app.sum(10, 20);
+        assertEquals(expected,actual);
     }
+    
+    @Test
+    public void testSumWithNegative() {
+        App app=new App();
+        int expected=-30;
+        int actual=app.sum(-10,-20);
+        assertEquals(expected,actual);
+    }
+    
+    @Test
+    public void testSumWithMixedValues() {
+        App app=new App();
+        int expected=10;
+        int actual=app.sum(-10,+20);
+        assertEquals(expected,actual);
+    }
+
+
 }
