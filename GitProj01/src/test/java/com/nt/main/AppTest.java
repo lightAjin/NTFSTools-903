26cf2d5783458a5cd1eedc7df41afc4266e6d8c5
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-19 20:49:02 +0530

    improved task1-----By Tl

--- GitProj01/src/test/java/com/nt/main/AppTest.java
+++ GitProj01/src/test/java/com/nt/main/AppTest.java
@@ -36,6 +36,14 @@ public class AppTest {
         int actual=app.sum(-10,+20);
         assertEquals(expected,actual);
     }
+    
+    @Test
+    public void testSumWithZeros() {
+        App app=new App();
+        int expected=0;
+        int actual=app.sum(0,0);
+        assertEquals(expected,actual);
+    }
 
 
 }
