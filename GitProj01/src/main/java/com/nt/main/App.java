caf046c0647208efb57cf06f58766d2f17543268
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-21 20:42:54 +0530

    TL commit line 12

--- GitProj01/src/main/java/com/nt/main/App.java
+++ GitProj01/src/main/java/com/nt/main/App.java
@@ -9,7 +9,7 @@ public class App {
         App app=new App();
        System.out.println("Sum is amt33::"+app.sum(10, 20));
        System.out.println("Sum amount22 is ::"+app.sum(10, 20));
-
+       System.out.println("Sum amount44 is ::"+app.sum(10, 20));
         //int x=sum(10,20);
     }
 }
