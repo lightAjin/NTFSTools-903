5cbfcbb3fea6b1ada696f780883071135c9b4feb
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 11:43:19 +0530

    Dev1-Account opening logic

--- GitProj01/src/main/java/com/nt/netBanking/netBankingService.java
+++ GitProj01/src/main/java/com/nt/netBanking/netBankingService.java
@@ -1,5 +1,10 @@
 package com.nt.netBanking;
 
+import java.util.Random;
+
 public class netBankingService {
 
+	public String openAccount(String aadhar,double initialAmot,String type) {
+		return "Account is open with acc number "+new Random().nextLong(50000000000000L);
+	}
 }
