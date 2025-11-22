89c75506c66992ddf275d0c1053f58b6fb6fe2eb
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 12:09:11 +0530

    Dev1-->close A/c

--- GitProj01/src/main/java/com/nt/netBanking/netBankingService.java
+++ GitProj01/src/main/java/com/nt/netBanking/netBankingService.java
@@ -7,4 +7,9 @@ public class netBankingService {
 	public String openAccount(String aadhar,double initialAmot,String type) {
 		return "Account is open with acc number "+new Random().nextLong(50000000000000L);
 	}
+	
+	
+	public String closeAccount() {
+		return "Account is closed";
+	}
 }
