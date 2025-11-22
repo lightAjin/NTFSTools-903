e408d7e594ed2332fcec6a7cf2dc337f96b56ec3
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 11:31:02 +0530

    Dev1-Changes done in NetBanking-Branch2

--- GitProj01/src/main/java/com/nt/banking/netBanking.java
+++ GitProj01/src/main/java/com/nt/banking/netBanking.java
@@ -2,7 +2,7 @@ package com.nt.banking;
 
 public class netBanking {
 
-//	public String netBanking(String name,double amount) {
-//		return "Bank name: "+name+"and Amount sended :"+amount;
-//	}
+	public String netBanking(String name,double amount) {
+		return "Bank name: "+name+"and Amount sended :"+amount;
+	}
 }
