80659ca082679025713f7b40d018dd392e215276
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 12:19:49 +0530

    Dev1-Transfer money operation

--- GitProj01/src/main/java/com/nt/netBanking/netBankingService.java
+++ GitProj01/src/main/java/com/nt/netBanking/netBankingService.java
@@ -8,6 +8,7 @@ public class netBankingService {
 		return "Account is open with acc number "+new Random().nextLong(50000000000000L);
 	}
 
+
 	
 	public String closeAccount() {
 		return "Account is closed";
@@ -17,6 +18,11 @@ public class netBankingService {
 	}
 	public String deposit(long accno,double amt) {
 		return amt+" is deposit from a/c number::"+accno;
+	}
+
+	public String transferMonry(long srcAccno,long descAccno,double amt) {
+		
+		return "Transferring"+ amt +"from "+srcAccno+" to "+descAccno;
 
 	}
 }
