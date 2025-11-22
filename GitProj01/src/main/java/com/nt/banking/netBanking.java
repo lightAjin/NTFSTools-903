1b6d29614e26bbbf3568344635602691f359d439
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 11:28:27 +0530

    TL created branch 2-NetBanking

---
+++ GitProj01/src/main/java/com/nt/banking/netBanking.java
@@ -0,0 +1,8 @@
+package com.nt.banking;
+
+public class netBanking {
+
+//	public String netBanking(String name,double amount) {
+//		return "Bank name: "+name+"and Amount sended :"+amount;
+//	}
+}
