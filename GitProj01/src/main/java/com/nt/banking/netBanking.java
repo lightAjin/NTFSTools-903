8141ee12e9c2cc63cbff3b4ac447d93f0e078a1f
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 11:32:28 +0530

    TL-NetBanking-brnach2-New Changes

--- GitProj01/src/main/java/com/nt/banking/netBanking.java
+++ GitProj01/src/main/java/com/nt/banking/netBanking.java
@@ -1,8 +1,13 @@
 package com.nt.banking;
 
+import java.util.Random;
+
 public class netBanking {
 
 	public String netBanking(String name,double amount) {
 		return "Bank name: "+name+"and Amount sended :"+amount;
+	}
+	public double checkBalance(String upiId) {
+		return new Random().nextInt(12000);
 	}
 }
