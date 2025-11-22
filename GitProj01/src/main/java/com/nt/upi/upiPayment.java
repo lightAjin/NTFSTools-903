f28f9cdfc86fb09487f6f983d542e8f709469658
Acer <Acer@DESKTOP-UJMLNVL>
2025-11-22 11:13:14 +0530

    TL enhancing Branch 1 code

--- GitProj01/src/main/java/com/nt/upi/upiPayment.java
+++ GitProj01/src/main/java/com/nt/upi/upiPayment.java
@@ -1,8 +1,14 @@
 package com.nt.upi;
 
+import java.util.Random;
+
 public class upiPayment {
 
-	public String doUpiPayment(String upiId,float amount) {
+	public String doUpiPayment(String upiId,double amount) {
 		return "UPI Payment for amount "+amount +"is done";
+	}
+	
+	public double checkBalance(String upiId) {
+		return new Random().nextInt(12000);
 	}
 }
